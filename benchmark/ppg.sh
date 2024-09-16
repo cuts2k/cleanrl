2a7303b785f812ba289b1158cbe2cbdb3135c118
cuts2k <cuts2k@gmail.com>
2024-09-16 18:00:57 +0100

    Shorter experiments

--- benchmark/ppg.sh
+++ benchmark/ppg.sh
@@ -1,7 +1,7 @@
 # export WANDB_ENTITY=openrlbenchmark
 
 xvfb-run -a python -m cleanrl_utils.benchmark \
-    --env-ids starpilot bossfight bigfish \
-    --command "python cleanrl/ppg_procgen.py --track" \
-    --num-seeds 3 \
-    --workers 1
+    --env-ids starpilot bossfight \
+    --command "python cleanrl/ppg_procgen.py --track --wandb_project_name $GIT_BRANCH" \
+    --num-seeds 2 \
+    --workers 2
