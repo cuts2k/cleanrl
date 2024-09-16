5accc814e10b2058e893c53966b6ed719d0148d9
cuts2k <cuts2k@gmail.com>
2024-09-16 12:13:09 +0100

    Setup for runpod

--- benchmark/ppg.sh
+++ benchmark/ppg.sh
@@ -1,8 +1,7 @@
 # export WANDB_ENTITY=openrlbenchmark
 
-poetry install -E procgen
-xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids starpilot bossfight bigfish \
-    --command "poetry run python cleanrl/ppg_procgen.py --track --capture_video" \
+    --command "python cleanrl/ppg_procgen.py --track" \
     --num-seeds 3 \
     --workers 1
